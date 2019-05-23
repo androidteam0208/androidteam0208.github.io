cef25a8cc4cb5b4f72a3d843678e16bffdbdc1fd
TranLoc0397 <loctran0397@gmail.com>
2019-05-23 08:22:43 +0700

    done menu

--- js/index.js
+++ js/index.js
@@ -1,61 +1,71 @@
 var menuicon = document.getElementById("menuicon");
 var check = false;
-menuicon.onclick= function() {checkIcon()};
+menuicon.onclick = function () {
+    checkIcon()
+};
+
 function checkIcon() {
-    if(!check){
+    if (!check) {
         showList();
-        check= true;
-    }
-    else {
+        check = true;
+    } else {
         hiddeList();
-        check= false;
+        check = false;
     }
 }
-function  showList() {
+
+function showList() {
     var navbar = document.getElementById("nav");
     navbar.classList.add("navbarList");
     navbar.classList.remove("navbar");
 }
-function  hiddeList() {
+
+function hiddeList() {
     var listmav = document.getElementById("nav");
     listmav.classList.add("navbar");
     listmav.classList.remove("navbarList");
+    $('.dropbtn').removeClass('active');
+    $('.show').removeClass('show');
 }
 
-document.getElementsByTagName("BODY")[0].onresize = function() {getWidth()};
+document.getElementsByTagName("BODY")[0].onresize = function () {
+    getWidth()
+};
 
 function getWidth() {
     let x = screen.width;
-    if(x>1024){
+    if (x > 1024) {
         hiddeList();
-        }
-    else{
-        if(this.check === true){
+    } else {
+        if (this.check === true) {
             this.check = !this.check;
             checkIcon();
         }
     }
 }
+
 //add vent click item menu to show dropdown
 function addEventClick() {
     let dropBtn1 = document.querySelectorAll(".dropbtn");
     for (const btn of dropBtn1) {
         btn.addEventListener('click', function (event) {
-            let dropDownDiv = this.parentNode.querySelector("div .dropdown-content");
-            dropDownDiv.classList.toggle('show');
+            let x = screen.width;
+            if (x < 1024) {
+                if (!$(event.target).hasClass('active')) {
+                    $('.dropbtn.active').removeClass('active');
+                    $('.dropbtn + .show').removeClass('show');
+                }
+                let dropDownDiv = this.parentNode.querySelector("div .dropdown-content");
+                dropDownDiv.classList.toggle('show');
+                btn.classList.toggle('active');
+            }
         })
     }
+
 }
+
 addEventClick();
-//when click out item menu with class dropbtn -> hide dropdown content
-window.addEventListener("click", function(event){
-    let contents = document.getElementsByClassName('dropdown-content');
-    if (!event.target.matches(".dropbtn")){
-        for (let i = 0; i < contents.length; i++) {
-            contents[i].classList.remove("show");
-        }
-    }
-});
+
 
 
 
